613201849627a693a3b417a3a88be9476d38448d
abbaswork <abbaskhanwork@gmail.com>
2021-05-02 22:41:49 -0400

    render page title

--- client/src/layouts/dashboard/dashboard-layout.js
+++ client/src/layouts/dashboard/dashboard-layout.js
@@ -10,17 +10,18 @@ export default function DashboardLayout(props) {
     /* Hook that controls responsive sidebar toggle, default for mobile is off and true for desktop */
     const [sidebar, setSidebar] = useState(Number(window.innerWidth) > 480 ? true : false);
     const toggle = () => setSidebar(prevState => !prevState);
+    const page = props.children.props.location.pathname.split('/');
+
 
     /* Adjust sidebar width based on view port */
     const setVw = (Number(window.innerWidth) > 480 ? '18rem' : '100vw');
-    console.log('dashboard: ', props);
 
     return (
         <Container className="h-100" fluid>
             <Sidebar sidebar={sidebar} toggle={toggle} setVw={setVw} />
             <Container fluid id="main" style={{ marginLeft: sidebar ? setVw : '0px' }}>
-                <PageHeader className="mb-5" toggle={toggle} sidebar={sidebar}
-                    title={'Dashboard'}
+                <PageHeader toggle={toggle} sidebar={sidebar}
+                    title={page[1].charAt(0).toUpperCase() + page[1].slice(1)}
                 />
                 {props.children}
             </Container>
