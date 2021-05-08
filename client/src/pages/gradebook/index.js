44df2e66c4b5054a75cc7e8de1f31f40d7de15ee
abbaswork <abbaskhanwork@gmail.com>
2021-05-08 11:32:56 -0400

    created gradebook page + component

---
+++ client/src/pages/gradebook/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+
+    return (
+      <>
+        <h1>Gradebook</h1>
+      </>
+    );
+  }
+}
+
+
